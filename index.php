21a3b0cc3a7479884117bfe4dc8d4d7ff20ed3fb
Milan Bancevic <m.bancevic@gmail.com>
2021-09-28 11:43:49 +0200

    DB connection fix

--- index.php
+++ index.php
@@ -1,12 +1,14 @@
 <?php
 
-use App\models\Intern;
+// use App\models\Intern.php;
+
+include_once 'src/config/Database.php';
 
 require_once realpath("vendor/autoload.php");
 
 echo "Index page";
 
-$intern = new Intern();
-
+$obj = new Database();
+$obj->connect();
 
 ?>
--- src/config/Database.php
+++ src/config/Database.php
@@ -2,10 +2,10 @@
 
 class Database {
     //DB params
-    private $host = 'localhost';
-    private $db_name = 'quantox-02';
-    private $username = 'root';
-    private $password = 'root';
+    private $host = "127.0.0.1";
+    private $db_name = "quantox-02";
+    private $username = "root";
+    private $password = "root";
     private $conn;
 
     //DB Connect
@@ -13,11 +13,11 @@ class Database {
         $this->conn = null;
 
         try {
-            $this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->db_name,
-            $this->username, $this->password);
-            $this->conn->serAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $dsn = "mysql:host=" . $this->host . ";dbname=" . $this->db_name;
+            $this->conn = new PDO($dsn, $this->username, $this->password);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch(PDOException $e) {
-            echo 'Connection Error: '. $e->getMessage();
+            echo "Connection Error: ". $e->getMessage();
         }
 
         return $this->conn;
